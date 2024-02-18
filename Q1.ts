7b95315a3e0859d677faf9d28b8e6e98c263bc98
Faiza Rao <159902088+faizarao2@users.noreply.github.com>
2024-02-18 18:41:04 +0500

    Add files via upload

---
+++ Q1.ts
@@ -0,0 +1,6 @@
+// Install [Node.js](https://nodejs.org/en/),[Typescript](https//www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
+
+// 1. https://code.visualstudio.com/download for (vs code)
+// 2. nodejs.org/en/download for (nodejs)
+// 3. npm i -g typescript  for (typescript)
+// 4. tsc -- init  for (tsconfig.json)
